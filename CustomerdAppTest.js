022e266dd820c9817bf2b0e9239fb3f9bdb6ea1b
lifveras <60801273+lifveras@users.noreply.github.com>
2022-02-16 12:35:35 -0300

    Update CustomerdAppTest.js

--- CustomerdAppTest.js
+++ CustomerdAppTest.js
@@ -1,17 +1,19 @@
 const accountConsumerAddress = "3MwrbksXinhUhJ9ewTxuxiPfKVsTx9GCbYC";
 const accountAliceAddress = '3N2pV6jPT2bdk2JNSchy3hgNfxhTm1gqhhu';
 const accountAliceSeed = 'system evil flag chair drive picture bar deny supply attitude toast toss cargo equip awesome';
+const accountBobAddress = '3N4MnQJWtixd7XMVtDSmWmfLj8M5PvLPHZo';
+const accountBobSeed = 'crawl manual raise save visa rather spice split still unique coffee wash forest wear earth';
 
 it('Buys item A from dApp', async () => {
     let txObjectSigned = invokeScript(
         {
             dApp: accountConsumerAddress,
             call:{
-                "function": "customerBuyA",
+                "function": "customerBuyProduct",
                 "args": [
                     {
-                        "type": "integer",
-                        "value": 200
+                        "type": "string",
+                        "value": "item_A"
                     }
                 ]
             },
@@ -19,7 +21,7 @@ it('Buys item A from dApp', async () => {
                 {amount:200, assetId: null}
             ]
         },
-        accountAliceSeed
+        accountBobSeed
     )
 
     let tx = await broadcast(txObjectSigned);
@@ -31,11 +33,11 @@ it('Buys item B from dApp', async () => {
         {
             dApp: accountConsumerAddress,
             call:{
-                "function": "customerBuyB",
+                "function": "customerBuyProduct",
                 "args": [
                     {
-                        "type": "integer",
-                        "value": 100
+                        "type": "string",
+                        "value": "item_B"
                     }
                 ]
             },
